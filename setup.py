269bf261b5108f7f0365fda02ac6efe36ede4ed5
Vitaly R <mortiv16@gmail.com>
2021-02-20 18:49:23 +0300

    make package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+requirements = [
+]
+
+setup(name='illusion',
+      version='0.1',
+      py_modules=['illusion'],
+      install_requires=requirements,
+      extras_require={
+      },
+      scripts=[
+      ],
+      )
